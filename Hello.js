2afbff9f1eca987592a85ebb1d66adf80b60f178
WaTness <liu060810@gmail.com>
2022-11-12 01:12:50 +0800

    20221111

---
+++ Hello.js
@@ -0,0 +1,12 @@
+function Mozart(){
+    window.alert("歡迎來到莫札特世界")
+}
+function Beethoven(){
+    window.alert("HIHI")
+}
+function Bach(){
+    window.alert("HIHI")
+}
+function Haydn(){
+    window.alert("HIHI")
+}
